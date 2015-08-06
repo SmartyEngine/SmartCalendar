64a55ba3b6fac1ca6169e4390034da5cfe814fec
Starushyk <starushykart@gmail.com>
2015-08-06 15:18:06 +0300

    Init

---
+++ SmartCalendar/SmartCalendar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmartCalendar.Startup))]
+namespace SmartCalendar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
